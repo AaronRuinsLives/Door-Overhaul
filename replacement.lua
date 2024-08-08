64de0f234e64e86e91ffc318cb725b69b85dff50
Aaron <aarongnzlz07@gmail.com>
2024-08-07 23:39:13 -0400

    Gave boss door unique highlights and changed some file names

--- replacement.lua
+++ replacement.lua
@@ -9,14 +9,14 @@ local normalRoomFilenames = {
     --[[Depths]] [7] = { default = "03_rd_depths_a", variants = {"03_rd_depths_a"}, anm2 = "rd_default", allowFlip = true },
     --[[Necropolis]] [8] = { default = "03a_rd_necropolis_a", variants = {"03a_rd_necropolis_a"}, anm2 = "rd_default", allowFlip = true },
     --[[Dank Depths]] [9] = { default = "03b_rd_dank_a", variants = {"03b_rd_dank_a"}, anm2 = "rd_default", allowFlip = true },
-    --[[Cathedral]] [15] = { default = "05_rd_cathedral_a", variants = {"05_rd_cathedral_a"}, anm2 = "rd_default_lit", allowFlip = true },
+    --[[Cathedral]] [15] = { default = "05_rd_cathedral_a", variants = {"05_rd_cathedral_a"}, anm2 = "rd_cathedral", allowFlip = true },
 }
 
 local roomFilenames = {
     --[[Shop]] [2] = { default = "00_rd_shopdoor", anm2 = "rd_default" },
     --[[Treasure]] [4] = { default = "00_rd_treasuredoor", anm2 = "rd_default" },
     --[[Treasure]] [-4] = { default = "00_rd_treasuredoor_greed", anm2 = "rd_default" },
-    --[[Boss]] [5] = { default = "00_rd_bossdoor", anm2 = "rd_default_lit" },
+    --[[Boss]] [5] = { default = "00_rd_bossdoor", anm2 = "rd_boss" },
     --[[Curse]] [10] = { default = "00_rd_cursedoor", anm2 = "rd_default" },
 }
 
